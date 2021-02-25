e5dcdc0bec15344386e4112cbb92116bfa58ad7f
Tony Guan <t9guan@ucsd.edu>
2021-02-25 14:35:51 -0800

    lab 8

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,3 +1,13 @@
+exports.addRSVP = function(request, response) {
+  var rsvpEmail = request.body.rsvpEmail;
+  // This will print to your terminal whenever the POST request is made
+  console.log(rsvpEmail);
+  data.rsvp.push(rsvpEmail); // Add to current data
+  // Let us send back some data so that
+  // browser knows it worked
+  response.send(rsvpEmail);
+  };  
+
 var data = {
   rsvp: ['ixd@ucsd.edu']
 };
